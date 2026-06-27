a680119fc631b3a31ec61f22ce8e7e1f3cd83be2
deepaklalvp <deepak@lithospos.com>
2026-06-27 16:51:28 +0530

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -13,23 +13,52 @@ document.addEventListener("DOMContentLoaded", async () => {
     const q = query(collection(db, "users"), orderBy("points", "desc"));
     const snap = await getDocs(q);
 
-    let rank = 1;
+    let users = [];
 
     snap.forEach(doc => {
+        users.push(doc.data());
+    });
 
-        const data = doc.data();
+    board.innerHTML = "";
 
+    // 🏆 TOP 3 PODIUM
+    const top3 = users.slice(0, 3);
+
+    const podium = document.createElement("div");
+    podium.classList.add("podium");
+
+    podium.innerHTML = `
+        <div class="podium-row silver">
+            🥈 <span>${top3[1]?.name || "-"}</span>
+            <b>${top3[1]?.points || 0}</b>
+        </div>
+
+        <div class="podium-row gold">
+            🥇 <span>${top3[0]?.name || "-"}</span>
+            <b>${top3[0]?.points || 0}</b>
+        </div>
+
+        <div class="podium-row bronze">
+            🥉 <span>${top3[2]?.name || "-"}</span>
+            <b>${top3[2]?.points || 0}</b>
+        </div>
+    `;
+
+    board.appendChild(podium);
+
+    // 🔻 Rest of leaderboard
+    const rest = users.slice(3);
+
+    rest.forEach((u, i) => {
         const row = document.createElement("div");
         row.classList.add("row");
 
         row.innerHTML = `
-            <div class="rank">#${rank}</div>
-            <div class="name">${data.name}</div>
-            <div class="points">${data.points || 0} pts</div>
+            <div class="rank">#${i + 4}</div>
+            <div class="name">${u.name}</div>
+            <div class="points">${u.points || 0}</div>
         `;
 
         board.appendChild(row);
-        rank++;
     });
-
 });
